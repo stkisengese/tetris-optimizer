c02199842e33ff4c3221512256dfbe9805f415b3
skisenge <skisengese@gmail.com>
2025-07-16 02:49:10 +0300

    feat: add args check

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . <input_file>")
+		os.Exit(1)
+	}
+	
+	filename := os.Args[1]
+	
+	fmt.Printf("Input file: %s\n", filename)
+	fmt.Println("Full implementation coming in next issues!")
+}
