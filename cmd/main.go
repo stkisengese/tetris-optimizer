83e48f630f56ad7a61bb99b427ad162f9c78820f
skisenge <skisengese@gmail.com>
2025-07-18 18:13:55 +0300

    chore: refactor main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
-
-	"github.com/stkisengese/tetris-optimizer/internal/parser"
-	"github.com/stkisengese/tetris-optimizer/internal/solver"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . <input_file>")
-		os.Exit(1)
+	result := RunApp(os.Args, os.Stdout)
+	if result.ExitCode != 0 {
+		os.Exit(result.ExitCode)
 	}
-
-	filename := os.Args[1]
-
-	// Parse tetrominoes from file
-	tetrominoes, err := parser.ReadFile(filename)
-	if err != nil {
-		fmt.Println("ERROR")
-		os.Exit(1)
-	}
-
-	// Solve the tetris puzzle
-	result, err := solver.SolveOptimal(tetrominoes)
-	if err != nil {
-		fmt.Println("ERROR")
-		os.Exit(1)
-	}
-
-	// Check if solution was found
-	if !result.Success {
-		fmt.Println("ERROR")
-		os.Exit(1)
-	}
-
-	// Print the solution
-	fmt.Print(result.Grid.String())
 }
